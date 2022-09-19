8bc13f4367ec05881011ee8384833a436b9d7648
Thomas Placidi <tomplacidi@thomass-mbp.home>
2022-09-19 15:05:29 +0100

    training setup

--- run.py
+++ run.py
@@ -3,11 +3,11 @@ from torch.utils.data import DataLoader
 from training import train
 from model import SimpleCNN
 
-cifar_dataset = CIFAR10Dataset()
+cifar_dataset = CIFAR10Dataset(directory_path='/Users/tomplacidi/env/datasets/cifar_10')
 dataloader = DataLoader(cifar_dataset, batch_size=100, shuffle=True)
 
 model = SimpleCNN()
 
-train(model=model, dataloader=dataloader, num_epochs=2)
+train(model=model, dataloader=dataloader, num_epochs=50)
 
 
--- training.py
+++ training.py
@@ -5,12 +5,14 @@ from tqdm import tqdm
 def train(model, dataloader, num_epochs=10, iteration_steps=100):
 
 	criterion = torch.nn.CrossEntropyLoss()
-	optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+	optimizer = torch.optim.SGD(model.parameters(), lr=0.005, momentum=0.9)
 
 	epoch_bar = tqdm(range(num_epochs))
 
 	for epoch in epoch_bar:
 		train_loss = 0
+		train_correct = 0
+		train_total = 0
 
 		for iteration, batch in enumerate(dataloader):
 			# read batch data
@@ -29,8 +31,13 @@ def train(model, dataloader, num_epochs=10, iteration_steps=100):
 
 			train_loss += loss.item()
 
-			if iteration % iteration_steps == 0:
-				epoch_bar.set_description('Epoch {0} | Iteration {1} | Loss {2}'.format(epoch, iteration, train_loss / iteration_steps))
+			train_correct += torch.sum(torch.argmax(outputs, dim=-1) == labels)
+			train_total += len(batch)
+
+			if iteration % iteration_steps == 0 and iteration > 0:
+				epoch_bar.set_description('Epoch {0} | Iteration {1} | Loss {2} | Accuracy {3}'.format(epoch, iteration, train_loss / train_total, train_correct / train_total))
 				train_loss = 0
+				train_correct = 0
+				train_total = 0
 
 	print('finished training')	
